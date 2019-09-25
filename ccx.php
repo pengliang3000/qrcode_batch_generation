9b5c68ad0755a2375ec40804d8b67371133be032
pengliang3000 <pengliang3000@163.com>
2019-09-25 20:23:12 +0800

    Update ccx.php

--- ccx.php
+++ ccx.php
@@ -1,14 +1,16 @@
 <?php
+//You can get qrcode library for php there - https://sourceforge.net/projects/phpqrcode/  
 include('D:/Users/phpqrcode/qrlib.php');
-//include('D:/Users/phpqrcode/config.php');
 
+//dir that save qrcode files 
 $outputDir = "d:/output/";
+//dir that save final files 
 $resultDir = "d:/output/result/";
 
-$startNumber = 2501;
+$startNumber = 1;
 $endNumber = 3000;
 //$endNumber = 222;
-$prefix = 'PF201910';
+$prefix = 'SWF201910';
 $baseImage = 'd:/baseImage.png';
 for($i=$startNumber;$i<=$endNumber;$i++){
 	$word = $prefix . str_pad($i,4,0,STR_PAD_LEFT);
@@ -21,34 +23,36 @@ for($i=$startNumber;$i<=$endNumber;$i++){
 }
 
 function create2wCode($word,$file){
+	/*1st : content of string 
+         *2st : where to save the qrcode file
+	 *3st QR_ECLEVEL_L
+	 *4st Qr code image size
+	 *5st qr code margin default:3, 0 means no margin
 	QRcode::png($word,$file,QR_ECLEVEL_L,8,0);
 }
 
 function compose2Image($src_file,$dst_file){	
-	global $resultDir;
-	//$path_1 = "C:/programs/php7/exec/base.png";
-	//$path_2 = 'http://tb1.bdstatic.com/tb/static-client/img/webpage/wap_code.png';
-	//$path_2 = "C:/programs/php7/exec/one.png";
+	global $resultDir;	
 	$image_1 = imagecreatefrompng($dst_file);
+	//Reserve the transparent area of the picture,or you will see background of the image fill in white 
 	imagesavealpha($image_1,true);
 	$image_2 = imagecreatefrompng($src_file);
 	imagesavealpha($image_2,true);
-//$cut = imagecreatetruecolor($src_w, $src_h);
-//imagecopy($cut, $dst_im, 0, 0, $dst_x, $dst_y, $src_w, $src_h);
-//imagecopy($cut, $src_im, 0, 0, $src_x, $src_y, $src_w, $src_h);
-//imagecopymerge($dst_im, $cut, $dst_x, $dst_y, 0, 0, $src_w, $src_h, $pct);
 	
 	imagecopymerge($image_1, $image_2, 94, 98, 0, 0, imagesx($image_2), imagesy($image_2), 100);
-	
+	//This font file is free,can use it for any purpose without any pay
 	$font = 'C:/Users/pengl/AppData/Local/Microsoft/Windows/Fonts/SourceHanSansCN-Medium.otf';
 	$black = imagecolorallocate($image_1, 0x00, 0x00, 0x00);
 	$info = pathinfo($src_file);
 	$merge = $resultDir . $info['basename'];
-	imagefttext($image_1, 18, 0, 66, 338, $black, $font, implode(' ',str_split($info['filename'])));//$info['filename']);
-
+	imagefttext($image_1, 18, 0, 66, 338, $black, $font, implode(' ',str_split($info['filename'])));
+        //output qr code files
 	$flag = imagepng($image_1,$merge);
 	imagedestroy($image_1);
 	imagedestroy($image_2);
+	
+	
+	//Modify  meta data of png files,I want  PNG files with resolution of 300.
 	$file = file_get_contents($merge);
 	$len = pack("N", 9);
 	$sign = pack("A*", "pHYs");
@@ -66,6 +70,6 @@ function compose2Image($src_file,$dst_file){
 	return $flag;
 }
 
-//$path_1 = 'https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png';
+
 
 ?>
